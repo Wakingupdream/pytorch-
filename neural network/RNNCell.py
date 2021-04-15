5b1e080bc2afa099ba1548ccf023dbbc3eedf2ed
WakingupDream <154351097@qq.com>
2021-04-15 10:14:49 +0800

    Finish RNNCell.

---
+++ neural network/RNNCell.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python
+import torch
+
+
+batch_size = 1
+seq_len = 3
+input_size = 4
+hidden_size = 2
+
+
+cell = torch.nn.RNNCell(input_size, hidden_size)
+
+
+dataset = torch.randn(seq_len, batch_size, input_size)
+hidden = torch.randn(batch_size, hidden_size)
+for idx, data in enumerate(dataset):
+    print("=" * 20, idx, "=" * 20)
+    print("Input size:", data.shape)
+    hidden = cell(data, hidden)
+    print("=" * 20, idx, "=" * 20)
+    print(hidden)
